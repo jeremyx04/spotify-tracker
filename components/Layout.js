816caa52aacf852c92eb185d5c32589c97e91556
Jeremy Xue <jer1506xue@gmail.com>
2022-07-02 23:44:47 -0400

    auth

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,20 @@
-import Head from 'next/head'
 import styles from './layout.module.css'
 import Link from 'next/link'
 
 export default function Layout({ children }){ 
     return(
-        <nav className={styles.navbar}>
-            <ul>
-                <Link href='/'>
-                    <li><a>Spotify Tracker</a></li>
-                </Link> 
-                <Link href='/'>
-                    <li><a>test2</a></li>
-                </Link> 
-            </ul>
-        </nav>
+        <>
+            <nav className={styles.navbar}>
+                <ul>
+                    <Link href='/'>
+                        <li><a>Spotify Tracker</a></li>
+                    </Link> 
+                    <Link href='/'> 
+                        <li><a>test2</a></li>
+                    </Link> 
+                </ul>
+            </nav>
+            <main> {children} </main>
+        </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import styles from '../styles/Home.module.css'
+import {useSession, signIn, signOut} from 'next-auth/react';
 
 export default function Index() {
-  return (
-    <div>
-      Hi
-    </div>
-  )
+  const { data: session,status } = useSession();
+  if(status === "authenticated"){
+    return (
+      <>
+        Signed in as {session?.token?.email}
+        <br />
+        <button onClick={signOut}> Sign out</button>
+      </>
+    )
+  }
+  if(status === "unauthenticated"){
+    return (
+      <>
+        Not logged in.
+        <br />
+        <button onClick={signIn}> Sign in </button>
+      </>
+    )
+  }
 }
